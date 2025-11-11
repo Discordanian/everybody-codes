5fa0cb9cfcefc544f039356454ef3f8e40767d5b
Kurt Schwind <kurt.schwind@gmail.com>
2025-11-10 20:11:48 -0500

    Day6-p3-b

--- 2025/godot/scripts/day_06.gd
+++ 2025/godot/scripts/day_06.gd
@@ -141,8 +141,45 @@ func part2(data: String, ans: LineEdit) -> void:
     
     ans.text = str(retval)
 
-
 func part3(data: String, ans: LineEdit) -> void:
+    const MAP_PERIOD: int = 1000
+    const MENTOR_WINDOW: int = 1000
+    
+    var retval: int = 0
+    
+    # First loop: process first MENTOR_WINDOW characters
+    for i:int in range(MENTOR_WINDOW):
+        if not data[i].to_upper() == data[i]:
+            var lowercase_char: String = data[i]
+            var uppercase_char: String = lowercase_char.to_upper()
+            var substring: String = data.substr(data.length() - MENTOR_WINDOW + i)
+            retval += substring.count(uppercase_char)
+        
+        if not data[data.length() - 1 - i].to_upper() == data[data.length() - 1 - i]:
+            var lowercase_char: String = data[data.length() - 1 - i]
+            var uppercase_char: String = lowercase_char.to_upper()
+            var substring: String = data.substr(0, MENTOR_WINDOW - i)
+            retval += substring.count(uppercase_char)
+    
+    retval *= MAP_PERIOD - 1
+    
+    # Second loop: process all characters
+    for i:int in range(data.length()):
+        if data[i].to_upper() == data[i]:
+            continue
+        
+        var lowercase_char: String = data[i]
+        var uppercase_char: String = lowercase_char.to_upper()
+        var start: int = max(0, i - MENTOR_WINDOW)
+        var length: int = min(data.length(), i + MENTOR_WINDOW + 1) - start
+        var substring: String = data.substr(start, length)
+        retval += substring.count(uppercase_char) * MAP_PERIOD
+    
+    ans.text = str(retval)
+
+
+
+func part3a(data: String, ans: LineEdit) -> void:
     var bigdata: String = ""
     var retval: int = 0
     for _i: int in range(1000):
