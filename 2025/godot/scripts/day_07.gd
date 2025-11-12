ed0e18458c1c71b0604a46a8553f86bd47617915
Kurt Schwind <kurt.schwind@gmail.com>
2025-11-12 10:48:12 -0600

    Day7

--- 2025/godot/scripts/day_07.gd
+++ 2025/godot/scripts/day_07.gd
@@ -73,7 +73,7 @@ func _on_example_pressed() -> void:
     
     part1(data1, example1)
     part2(data2, example2)
-    part3(data3, example3)
+    part3p(data3, example3)
     
 func _on_input_pressed() -> void:
     var path1: String = ECodes.input_path(year, day, 1)
@@ -92,7 +92,7 @@ func _on_input_pressed() -> void:
         part2(ECodes.string_from_file(path2), answer2)        
     
     if FileAccess.file_exists(path3):
-        part3(ECodes.string_from_file(path3), answer3)
+        part3p(ECodes.string_from_file(path3), answer3)
 
 
 func _on_main_pressed() -> void:
@@ -109,7 +109,7 @@ func _on_example_text_edit_3_text_changed() -> void:
 
 #endregion
 
-func valid_name(n: String, map: Dictionary) -> bool:
+func valid_puzzle_name(n: String, map: Dictionary) -> bool:
     for idx: int in range(n.length() - 1):
         var key: String = n[idx]
         var target: String = n[idx + 1]
@@ -120,7 +120,7 @@ func valid_name(n: String, map: Dictionary) -> bool:
     return true
 
 func number_possible(n: String, map: Dictionary) -> int:
-    if not valid_name(n, map):
+    if not valid_puzzle_name(n, map):
         return 0
     if n.length() > 11:
         return 0
@@ -130,8 +130,8 @@ func number_possible(n: String, map: Dictionary) -> int:
     var extra: PackedStringArray = map[last_letter]
     var sum: int = 0
     for c: String in extra:
-        var dance_mix_name: String = n + c
-        sum += number_possible(dance_mix_name, map)
+        var dance_mix_puzzle_name: String = n + c
+        sum += number_possible(dance_mix_puzzle_name, map)
     if n.length() > 6:
         sum += extra.size()
     return sum
@@ -139,7 +139,7 @@ func number_possible(n: String, map: Dictionary) -> int:
 
 func part1(data: String, ans: LineEdit) -> void:
     var lines: PackedStringArray = ECodes.string_to_lines(data.strip_edges())
-    var names: PackedStringArray = lines[0].split(",")
+    var puzzle_names: PackedStringArray = lines[0].split(",")
     var retval: String = ""
     var map: Dictionary = {}
     for i: int in range(lines.size()):
@@ -149,8 +149,8 @@ func part1(data: String, ans: LineEdit) -> void:
         var key: String = arrow[0]
         var val: PackedStringArray = arrow[1].split(",")
         map[key] = val
-    for n: String in names:
-        if valid_name(n, map):
+    for n: String in puzzle_names:
+        if valid_puzzle_name(n, map):
             retval = n  
     ans.text = retval
     
@@ -159,7 +159,7 @@ func part1(data: String, ans: LineEdit) -> void:
     
 func part2(data: String, ans: LineEdit) -> void:
     var lines: PackedStringArray = ECodes.string_to_lines(data.strip_edges())
-    var names: PackedStringArray = lines[0].split(",")
+    var puzzle_names: PackedStringArray = lines[0].split(",")
     var retval: int = 0
     var map: Dictionary = {}
     for i: int in range(lines.size()):
@@ -169,8 +169,8 @@ func part2(data: String, ans: LineEdit) -> void:
         var key: String = arrow[0]
         var val: PackedStringArray = arrow[1].split(",")
         map[key] = val
-    for idx: int in range(names.size()):
-        if valid_name(names[idx], map):
+    for idx: int in range(puzzle_names.size()):
+        if valid_puzzle_name(puzzle_names[idx], map):
             retval += (1 + idx)
     
     ans.text = str(retval)
@@ -178,9 +178,9 @@ func part2(data: String, ans: LineEdit) -> void:
 
 func part3(data: String, ans: LineEdit) -> void:
     var lines: PackedStringArray = ECodes.string_to_lines(data.strip_edges())
-    var names: PackedStringArray = lines[0].split(",")
+    var puzzle_names: PackedStringArray = lines[0].split(",")
     var map: Dictionary = {}
-    var name_set: Set = Set.new()
+    var puzzle_name_set: Set = Set.new()
     var q: Array[String] = []
     var seen: Array[String] = []
     for i: int in range(lines.size()):
@@ -192,8 +192,8 @@ func part3(data: String, ans: LineEdit) -> void:
         map[key] = val
 
 
-    for n: String in names:
-        if valid_name:
+    for n: String in puzzle_names:
+        if valid_puzzle_name:
             q.append(n)
     
     while not q.is_empty():
@@ -204,10 +204,76 @@ func part3(data: String, ans: LineEdit) -> void:
             continue
         seen.append(n)
         if n.length() > 6:
-            name_set.add(n)
+            puzzle_name_set.add(n)
         var last_char: String = n[n.length() - 1]
         if map.has(last_char):
             for next: String in map[last_char]:
                 q.append(n + next)
-    debug_print(name_set)
-    ans.text = str(name_set.size())
+    debug_print(puzzle_name_set)
+    ans.text = str(puzzle_name_set.size())
+    
+func combinations(puzzle_name: String, rules: Dictionary) -> Array[String]:
+    if puzzle_name.length() == 11:
+        return [puzzle_name]
+    
+    var last_char: String = puzzle_name[puzzle_name.length() - 1]
+    var letters: Array = rules.get(last_char, [])
+    
+    if letters.is_empty():
+        return [puzzle_name]
+    
+    var result: Array[String] = [puzzle_name]
+    
+    for c: String in letters:
+        var new_puzzle_name: String = puzzle_name + c
+        var recursive_results: Array[String] = combinations(new_puzzle_name, rules)
+        for r: String in recursive_results:
+            if not result.has(r):
+                result.append(r)
+    
+    return result
+
+
+func part3p(data: String, ans: LineEdit) -> void:
+    var lines: PackedStringArray = ECodes.string_to_lines(data.strip_edges())
+    var puzzle_names: PackedStringArray = lines[0].split(",")
+  
+    # Parse rules into Dictionary where key is String and value is Array[String]
+    var map: Dictionary = {}
+
+    for i: int in range(lines.size()):
+        if i < 2:
+            continue
+        var arrow: PackedStringArray = lines[i].split(" > ")
+        var key: String = arrow[0]
+        var val: PackedStringArray = arrow[1].split(",")
+        map[key] = val    
+    
+    # Build result set (using Array as Set equivalent)
+    var res: Array[String] = []
+    
+    for puzzle_name: String in puzzle_names:
+        # Check if puzzle_name follows the rules
+        var valid: bool = true
+        for i: int in range(puzzle_name.length() - 1):
+            var current_char: String = puzzle_name[i]
+            var next_char: String = puzzle_name[i + 1]
+            
+            var rule_chars: Array = map.get(current_char, [])
+            if not rule_chars.has(next_char):
+                valid = false
+                break
+        
+        if valid:
+            var combo_results: Array[String] = combinations(puzzle_name, map)
+            for r: String in combo_results:
+                if not res.has(r):
+                    res.append(r)
+    
+    # Count results with length >= 7
+    var count: int = 0
+    for r: String in res:
+        if r.length() >= 7:
+            count += 1
+    
+    ans.text = str(count)
