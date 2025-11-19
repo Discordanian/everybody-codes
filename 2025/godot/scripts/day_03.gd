b4409ab3426835bdaab3bb35a8d176f321d0deb3
Kurt Schwind <kurt.schwind@gmail.com>
2025-11-19 05:59:48 -0600

    Saving your bacon

--- 2025/godot/scripts/day_03.gd
+++ 2025/godot/scripts/day_03.gd
@@ -23,105 +23,105 @@ extends Control
 var example_path1: String
 var example_path2: String
 var example_path3: String
-       
+	   
 func setup_example() -> void:
-    var content1: String =  ECodes.string_from_file(example_path1)
-    var content2: String =  ECodes.string_from_file(example_path2)
-    var content3: String =  ECodes.string_from_file(example_path3)
-    exampleText.text = content1
-    exampleText2.text = content2
-    exampleText3.text = content3
+	var content1: String =  ECodes.string_from_file(example_path1)
+	var content2: String =  ECodes.string_from_file(example_path2)
+	var content3: String =  ECodes.string_from_file(example_path3)
+	exampleText.text = content1
+	exampleText2.text = content2
+	exampleText3.text = content3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    $MarginContainer/VBoxContainer/Header/Label.text = "Everybody Codes Day %02d" % day
-    example_path1 = ECodes.example_path(year,day, 1)
-    example_path2 = ECodes.example_path(year,day, 2)
-    example_path3 = ECodes.example_path(year, day, 3)
-    setup_example()
+	$MarginContainer/VBoxContainer/Header/Label.text = "Everybody Codes Day %02d" % day
+	example_path1 = ECodes.example_path(year,day, 1)
+	example_path2 = ECodes.example_path(year,day, 2)
+	example_path3 = ECodes.example_path(year, day, 3)
+	setup_example()
 
 
 func _on_example_text_edit_text_changed() -> void:
-    var file: FileAccess = FileAccess.open(example_path1, FileAccess.WRITE)
-    if file:
-        file.store_string(exampleText.text)
-        file.close()
-    else:
-        push_error("Error writing to " + example_path1)
-    
+	var file: FileAccess = FileAccess.open(example_path1, FileAccess.WRITE)
+	if file:
+		file.store_string(exampleText.text)
+		file.close()
+	else:
+		push_error("Error writing to " + example_path1)
+	
 func _on_example_text_edit_text_changed2() -> void:
-    var file: FileAccess = FileAccess.open(example_path2, FileAccess.WRITE)
-    if file:
-        file.store_string(exampleText2.text)
-        file.close()
-    else:
-        push_error("Error writing to " + example_path2)
+	var file: FileAccess = FileAccess.open(example_path2, FileAccess.WRITE)
+	if file:
+		file.store_string(exampleText2.text)
+		file.close()
+	else:
+		push_error("Error writing to " + example_path2)
 
 func _on_example_pressed() -> void:
-    print_debug("_on_example_pressed()")
-    var data1: String = ECodes.string_from_file(example_path1)
-    var data2: String = ECodes.string_from_file(example_path2)
-    var data3: String = ECodes.string_from_file(example_path3)
-    part1(data1, example1)
-    part2(data2, example2)
-    part3(data3, example3)
-    
+	print_debug("_on_example_pressed()")
+	var data1: String = ECodes.string_from_file(example_path1)
+	var data2: String = ECodes.string_from_file(example_path2)
+	var data3: String = ECodes.string_from_file(example_path3)
+	part1(data1, example1)
+	part2(data2, example2)
+	part3(data3, example3)
+	
 func _on_input_pressed() -> void:
-    var path1: String = ECodes.input_path(year, day, 1)
-    var path2: String = ECodes.input_path(year, day, 2)
-    var path3: String = ECodes.input_path(year, day, 3)
-    
-    answer1.text = "Input file not found"
-    answer2.text = "Input file not found"
-    answer3.text = "Input file not found"
-    
-    if FileAccess.file_exists(path1):
-        part1(ECodes.string_from_file(path1), answer1)
+	var path1: String = ECodes.input_path(year, day, 1)
+	var path2: String = ECodes.input_path(year, day, 2)
+	var path3: String = ECodes.input_path(year, day, 3)
+	
+	answer1.text = "Input file not found"
+	answer2.text = "Input file not found"
+	answer3.text = "Input file not found"
+	
+	if FileAccess.file_exists(path1):
+		part1(ECodes.string_from_file(path1), answer1)
 
-    if FileAccess.file_exists(path2):
-        part2(ECodes.string_from_file(path2), answer2)        
-    
-    if FileAccess.file_exists(path3):
-        part3(ECodes.string_from_file(path3), answer3)
+	if FileAccess.file_exists(path2):
+		part2(ECodes.string_from_file(path2), answer2)        
+	
+	if FileAccess.file_exists(path3):
+		part3(ECodes.string_from_file(path3), answer3)
 
 
 func _on_main_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/main_entry.tscn") 
+	get_tree().change_scene_to_file("res://scenes/main_entry.tscn") 
 
 
 func _on_example_text_edit_3_text_changed() -> void:
-    var file: FileAccess = FileAccess.open(example_path3, FileAccess.WRITE)
-    if file:
-        file.store_string(exampleText3.text)
-        file.close()
-    else:
-        push_error("Error writing to " + example_path3)
+	var file: FileAccess = FileAccess.open(example_path3, FileAccess.WRITE)
+	if file:
+		file.store_string(exampleText3.text)
+		file.close()
+	else:
+		push_error("Error writing to " + example_path3)
 
 #endregion
 
 func part1(data: String, ans: LineEdit) -> void:
-    var crates: Array[int] = ECodes.array_int_from_string(data)
-    var myset: Set = Set.new()
-    myset.add_all(crates)
-    var sum: int = 0
-    for s: int in myset:
-        sum += s
-    ans.text = str(sum)
-    
-    
+	var crates: Array[int] = ECodes.array_int_from_string(data)
+	var myset: Set = Set.new()
+	myset.add_all(crates)
+	var sum: int = 0
+	for s: int in myset:
+		sum += s
+	ans.text = str(sum)
+	
+	
 func part2(data: String, ans: LineEdit) -> void:
-    var crates: Array[int] = ECodes.array_int_from_string(data)
-    var myset: Set = Set.new()
-    myset.add_all(crates)
-    var sum: int = 0
-    var sorted: Array = myset.get_as_array()
-    sorted.sort()
-    for c:int in sorted.slice(0,20):
-        sum += c
-    ans.text = str(sum)
+	var crates: Array[int] = ECodes.array_int_from_string(data)
+	var myset: Set = Set.new()
+	myset.add_all(crates)
+	var sum: int = 0
+	var sorted: Array = myset.get_as_array()
+	sorted.sort()
+	for c:int in sorted.slice(0,20):
+		sum += c
+	ans.text = str(sum)
 
 
 func part3(data: String, ans: LineEdit) -> void:
-    var crates: Array[int] = ECodes.array_int_from_string(data)
-    var counter: Counter = Counter.new(crates)
-    ans.text = str(counter.most_common(1)[0][1])
+	var crates: Array[int] = ECodes.array_int_from_string(data)
+	var counter: Counter = Counter.new(crates)
+	ans.text = str(counter.most_common(1)[0][1])
--- 2025/godot/scripts/quests/Q02.gd
+++ 2025/godot/scripts/quests/Q02.gd
@@ -2,71 +2,71 @@ class_name Q02 extends RefCounted
 
 
 static func _complex_add(lhs: Array[int], rhs: Array[int]) -> Array[int]:
-    var retval: Array[int] = []
-    retval.append(lhs[0] + rhs[0])
-    retval.append(lhs[1] + rhs[1])
-    return retval
-    
+	var retval: Array[int] = []
+	retval.append(lhs[0] + rhs[0])
+	retval.append(lhs[1] + rhs[1])
+	return retval
+	
    
 static func _complex_multiply(lhs: Array[int], rhs: Array[int]) -> Array[int]:
-    var retval: Array[int] = []
-    retval.append(rhs[0] * lhs[0] - rhs[1] * lhs[1])
-    retval.append(rhs[0] * lhs[1] + rhs[1] * lhs[0])
-    return retval
+	var retval: Array[int] = []
+	retval.append(rhs[0] * lhs[0] - rhs[1] * lhs[1])
+	retval.append(rhs[0] * lhs[1] + rhs[1] * lhs[0])
+	return retval
 
 
 static func _complex_division(rhs: Array[int], lhs: Array[int]) -> Array[int]:
-    var retval: Array[int] = []
-    @warning_ignore_start("integer_division")
-    retval.append(rhs[0]/lhs[0])
-    retval.append(rhs[1]/lhs[1])
-    @warning_ignore_restore("integer_division")
-    return retval
+	var retval: Array[int] = []
+	@warning_ignore_start("integer_division")
+	retval.append(rhs[0]/lhs[0])
+	retval.append(rhs[1]/lhs[1])
+	@warning_ignore_restore("integer_division")
+	return retval
 
 
 static func _cycle(r: Array[int], a: Array[int]) -> Array[int]:
-    var retval: Array[int] = _complex_multiply(r, r)
-    retval = _complex_division(retval, [10,10])
-    retval = _complex_add(retval, a)
-    return retval
+	var retval: Array[int] = _complex_multiply(r, r)
+	retval = _complex_division(retval, [10,10])
+	retval = _complex_add(retval, a)
+	return retval
  
 static func _cycle64(r: Array[int], a: Array[int]) -> Array[int]:
-    var retval: Array[int] = _complex_multiply(r, r)
-    retval = _complex_division(retval, [100_000,100_000])
-    retval = _complex_add(retval, a)
-    return retval     
+	var retval: Array[int] = _complex_multiply(r, r)
+	retval = _complex_division(retval, [100_000,100_000])
+	retval = _complex_add(retval, a)
+	return retval     
 
 static func _good_cycle(a: Array[int]) -> int:
-    var r: Array[int] = [0,0]
-    for _i: int in range(100):
-        r = _cycle64(r, a)
-        if abs(r[0]) > 1_000_000 or abs(r[1]) > 1_000_000:
-            return 0
-    return 1
+	var r: Array[int] = [0,0]
+	for _i: int in range(100):
+		r = _cycle64(r, a)
+		if abs(r[0]) > 1_000_000 or abs(r[1]) > 1_000_000:
+			return 0
+	return 1
 
 
 static func part1(data: String) -> String:
-    var a: Array[int] = ECodes.array_int_from_string(data)
-    var r: Array[int] = [0, 0]
-    r = _cycle(r,a)
-    r = _cycle(r,a)
-    r = _cycle(r,a)
-    return "[%d,%d]" % [r[0], r[1]]
-    
+	var a: Array[int] = ECodes.array_int_from_string(data)
+	var r: Array[int] = [0, 0]
+	r = _cycle(r,a)
+	r = _cycle(r,a)
+	r = _cycle(r,a)
+	return "[%d,%d]" % [r[0], r[1]]
+	
 
 static func part2(data: String) -> String:
-    var a: Array[int] = ECodes.array_int_from_string(data)
-    var good: int = 0
-    for y: int in range(101):
-        for x: int in range(101):
-            good += _good_cycle(_complex_add(a, [x*10, y*10]))
-    return str(good)
+	var a: Array[int] = ECodes.array_int_from_string(data)
+	var good: int = 0
+	for y: int in range(101):
+		for x: int in range(101):
+			good += _good_cycle(_complex_add(a, [x*10, y*10]))
+	return str(good)
 
 
 static func part3(data: String) -> String:
-    var a: Array[int] = ECodes.array_int_from_string(data)
-    var good: int = 0
-    for y: int in range(1001):
-        for x: int in range(1001):
-            good += _good_cycle(_complex_add(a, [x, y]))
-    return str(good)
+	var a: Array[int] = ECodes.array_int_from_string(data)
+	var good: int = 0
+	for y: int in range(1001):
+		for x: int in range(1001):
+			good += _good_cycle(_complex_add(a, [x, y]))
+	return str(good)
