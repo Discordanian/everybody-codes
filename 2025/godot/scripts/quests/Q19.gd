cc5252a40a965202af6b508b7e22aa16bf031a68
Kurt Schwind <kurt.schwind@gmail.com>
2025-11-27 18:16:00 -0600

    Quest Day 19

--- 2025/godot/scripts/quests/Q19.gd
+++ 2025/godot/scripts/quests/Q19.gd
@@ -1,10 +1,11 @@
 class_name Q19 extends RefCounted
 
+const BIG_NUMBER: int = 1234567890
+
 func part1(data: String) -> String:
     var lines: PackedStringArray = data.split("\n")
     var r: Array[Array] = []
     
-    # Parse input
     var regex: RegEx = RegEx.new()
     regex.compile(r"-?\d+")
     
@@ -18,13 +19,13 @@ func part1(data: String) -> String:
         if nums.size() > 0:
             r.append(nums)
     
-    # Build D dictionary
+
     var D: Dictionary = {} # Dictionary[int, Array]
     for row: Array in r:
-        var row_arr: Array = row as Array
-        var a: int = row_arr[0]
-        var b: int = row_arr[1]
-        var c: int = row_arr[2]
+
+        var a: int = row[0]
+        var b: int = row[1]
+        var c: int = row[2]
         
         if not D.has(a):
             D[a] = []
@@ -33,30 +34,26 @@ func part1(data: String) -> String:
     # Find max key
     var e: int = 0
     for key: int in D.keys():
-        var k: int = key as int
-        if k > e:
-            e = k
+        if key > e:
+            e = key
     
-    # Build H dictionary (height ranges)
     var H: Dictionary = {} # Dictionary[int, Array[int]]
-    for a_int: int in D.keys():
-        # var a_int: int = a as int
-        H[a_int] = []
-        var l: Array = D[a_int]
-        for item: Array in l:
-            var pair: Array = item as Array
+    for a: int in D.keys():
+        H[a] = []
+        var l: Array = D[a]
+        for pair: Array in l:
             var y: int = pair[0]
             var h: int = pair[1]
             for i: int in range(y, y + h):
-                H[a_int].append(i)
+                H[a].append(i)
     
-    # Sort keys for iteration
+
     var sorted_keys: Array[int] = []
     for key: int in H.keys():
-        sorted_keys.append(key as int)
+        sorted_keys.append(key)
     sorted_keys.sort()
     
-    # Dynamic programming
+
     var q: Dictionary = {} # Dictionary[int, int]
     q[0] = 0
     var x: int = 0
@@ -69,12 +66,10 @@ func part1(data: String) -> String:
         var l1: Array = H[x1]
         
         for h1: int in q.keys():
-            var h1_int: int = h1 as int
-            var v: int = q[h1_int]
+            var v: int = q[h1]
             
-            for h2_var: int in l1:
-                var h2: int = h2_var as int
-                var d: int = abs(h1_int - h2)
+            for h2: int in l1:
+                var d: int = abs(h1 - h2)
                 
                 if d > w:
                     continue
@@ -82,18 +77,17 @@ func part1(data: String) -> String:
                     continue
                 
                 @warning_ignore("integer_division")
-                var m: int = v + (w + h2 - h1_int) / 2
+                var m: int = v + (w + h2 - h1) / 2
                 
                 if not q2.has(h2):
-                    q2[h2] = 1000000000
+                    q2[h2] = BIG_NUMBER
                 q2[h2] = min(m, q2[h2])
         
         q = q2
     
     # Find minimum value
-    var min_val: int = 1000000000
-    for val: int in q.values():
-        var v: int = val as int
+    var min_val: int = BIG_NUMBER
+    for v: int in q.values():
         if v < min_val:
             min_val = v
     
