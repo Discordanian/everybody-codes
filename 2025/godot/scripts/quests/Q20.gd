2c7ed2a1f76357ff23a1c1eb999c98d17780466d
Kurt Schwind <kurt.schwind@gmail.com>
2025-11-28 18:13:17 -0600

    Q20p2

--- 2025/godot/scripts/quests/Q20.gd
+++ 2025/godot/scripts/quests/Q20.gd
@@ -10,9 +10,9 @@ func adj(p: Array[int]) -> Array[Array]:
     result.append([i, j - 1])
     result.append([i, j + 1])
     
-    if j % 2 == 1: # ^
+    if j % 2 == 1: # point up
         result.append([i + 1, j - 1])
-    else: # v
+    else: # point down
         result.append([i - 1, j + 1])
     
     return result
@@ -20,7 +20,7 @@ func adj(p: Array[int]) -> Array[Array]:
 func part1(data: String) -> String:
     var lines: PackedStringArray = data.split("\n")
     
-    # Strip trailing dots from each line
+
     var grid: Array[String] = []
     for l: String in lines:
         if l.is_empty():
@@ -28,7 +28,7 @@ func part1(data: String) -> String:
         var stripped: String = l.rstrip(".").lstrip(".")
         grid.append(stripped)
     
-    # Build grid dictionary using string keys
+
     var grid_dict: Dictionary = {} # Dictionary[String, String]
     for i: int in range(grid.size()):
         var row: String = grid[i]
@@ -37,13 +37,13 @@ func part1(data: String) -> String:
             var key: String = str(i) + "," + str(j)
             grid_dict[key] = cell
     
-    # Count adjacent T pairs
+
     var total: int = 0
     for key: String in grid_dict.keys():
         var cell: String = grid_dict[key]
         
         if cell == 'T':
-            # Parse the key back to coordinates
+
             var parts: PackedStringArray = key.split(",")
             var i: int = int(parts[0])
             var j: int = int(parts[1])
@@ -58,7 +58,72 @@ func part1(data: String) -> String:
 
 
 func part2(data: String) -> String:
-    return str(data.length() + 123456)
+    var lines: PackedStringArray = data.split("\n")
+    
+    # Strip dots from both ends of each line
+    var grid: Array[String] = []
+    for l: String in lines:
+        if l.is_empty():
+            continue
+        var stripped: String = l.lstrip(".").rstrip(".")
+        grid.append(stripped)
+    
+    # Build grid dictionary and find start/end positions
+    var grid_dict: Dictionary = {} # Dictionary[String, String]
+    var start: String = ""
+    var end: String = ""
+    
+    for i: int in range(grid.size()):
+        var row: String = grid[i]
+        for j: int in range(row.length()):
+            var cell: String = row[j]
+            var key: String = str(i) + "," + str(j)
+            
+            if cell == 'S':
+                start = key
+                cell = 'T'
+            elif cell == 'E':
+                end = key
+                cell = 'T'
+            
+            grid_dict[key] = cell
+    
+    # Initialize costs dictionary
+    var costs: Dictionary = {} # Dictionary[String, int]
+    for key: String in grid_dict.keys():
+        var cell: String = grid_dict[key]
+        if cell == 'T':
+            costs[key] = 1000000000
+    
+    costs[start] = 0
+    
+    # BFS with queue
+    var q: Array[String] = [start]
+    var in_q: Dictionary = {} # Dictionary[String, bool] - using as a set
+    in_q[start] = true
+    
+    while q.size() > 0:
+        var p: String = q.pop_front()
+        in_q.erase(p)
+        
+        var cost: int = costs[p] + 1
+        
+        # Parse position
+        var parts: PackedStringArray = p.split(",")
+        var i: int = int(parts[0])
+        var j: int = int(parts[1])
+        
+        var adjacent: Array[Array] = adj([i, j])
+        for a: Array in adjacent:
+            var a_key: String = str(a[0]) + "," + str(a[1])
+            
+            if costs.has(a_key) and cost < costs[a_key]:
+                costs[a_key] = cost
+                if not in_q.has(a_key):
+                    q.append(a_key)
+                    in_q[a_key] = true
+    
+    return str(costs[end])
 
 
 func part3(data: String) -> String:
