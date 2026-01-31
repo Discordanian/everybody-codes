6b03df646c0744d228ae038b70830ec2b1ab662e
Kurt Schwind <kurt.schwind@gmail.com>
2026-01-31 10:20:26 -0600

    day_04.gd

--- 2025/godot/scripts/day_04.gd
+++ 2025/godot/scripts/day_04.gd
@@ -25,108 +25,108 @@ var example_path2: String
 var example_path3: String
        
 func setup_example() -> void:
-    var content1: String =  ECodes.string_from_file(example_path1)
-    var content2: String =  ECodes.string_from_file(example_path2)
-    var content3: String =  ECodes.string_from_file(example_path3)
-    exampleText.text = content1
-    exampleText2.text = content2
-    exampleText3.text = content3
+	var content1: String =  ECodes.string_from_file(example_path1)
+	var content2: String =  ECodes.string_from_file(example_path2)
+	var content3: String =  ECodes.string_from_file(example_path3)
+	exampleText.text = content1
+	exampleText2.text = content2
+	exampleText3.text = content3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    $MarginContainer/VBoxContainer/Header/Label.text = "Everybody Codes Day %02d" % [day]
-    example_path1 = ECodes.example_path(year,day, 1)
-    example_path2 = ECodes.example_path(year,day, 2)
-    example_path3 = ECodes.example_path(year, day, 3)
-    setup_example()
+	$MarginContainer/VBoxContainer/Header/Label.text = "Everybody Codes Day %02d" % [day]
+	example_path1 = ECodes.example_path(year,day, 1)
+	example_path2 = ECodes.example_path(year,day, 2)
+	example_path3 = ECodes.example_path(year, day, 3)
+	setup_example()
 
 
 func _on_example_text_edit_text_changed() -> void:
-    var file: FileAccess = FileAccess.open(example_path1, FileAccess.WRITE)
-    if file:
-        file.store_string(exampleText.text)
-        file.close()
-    else:
-        push_error("Error writing to " + example_path1)
+	var file: FileAccess = FileAccess.open(example_path1, FileAccess.WRITE)
+	if file:
+		file.store_string(exampleText.text)
+		file.close()
+	else:
+		push_error("Error writing to " + example_path1)
     
 func _on_example_text_edit_text_changed2() -> void:
-    var file: FileAccess = FileAccess.open(example_path2, FileAccess.WRITE)
-    if file:
-        file.store_string(exampleText2.text)
-        file.close()
-    else:
-        push_error("Error writing to " + example_path2)
+	var file: FileAccess = FileAccess.open(example_path2, FileAccess.WRITE)
+	if file:
+		file.store_string(exampleText2.text)
+		file.close()
+	else:
+		push_error("Error writing to " + example_path2)
 
 func _on_example_pressed() -> void:
-    print_debug("_on_example_pressed()")
-    var data1: String = ECodes.string_from_file(example_path1)
-    var data2: String = ECodes.string_from_file(example_path2)
-    var data3: String = ECodes.string_from_file(example_path3)
-    part1(data1, example1)
-    part2(data2, example2)
-    part3(data3, example3)
+	print_debug("_on_example_pressed()")
+	var data1: String = ECodes.string_from_file(example_path1)
+	var data2: String = ECodes.string_from_file(example_path2)
+	var data3: String = ECodes.string_from_file(example_path3)
+	part1(data1, example1)
+	part2(data2, example2)
+	part3(data3, example3)
     
 func _on_input_pressed() -> void:
-    var path1: String = ECodes.input_path(year, day, 1)
-    var path2: String = ECodes.input_path(year, day, 2)
-    var path3: String = ECodes.input_path(year, day, 3)
+	var path1: String = ECodes.input_path(year, day, 1)
+	var path2: String = ECodes.input_path(year, day, 2)
+	var path3: String = ECodes.input_path(year, day, 3)
     
-    answer1.text = "Input file not found"
-    answer2.text = "Input file not found"
-    answer3.text = "Input file not found"
+	answer1.text = "Input file not found"
+	answer2.text = "Input file not found"
+	answer3.text = "Input file not found"
     
-    if FileAccess.file_exists(path1):
-        part1(ECodes.string_from_file(path1), answer1)
+	if FileAccess.file_exists(path1):
+		part1(ECodes.string_from_file(path1), answer1)
 
-    if FileAccess.file_exists(path2):
-        part2(ECodes.string_from_file(path2), answer2)        
+	if FileAccess.file_exists(path2):
+		part2(ECodes.string_from_file(path2), answer2)        
     
-    if FileAccess.file_exists(path3):
-        part3(ECodes.string_from_file(path3), answer3)
+	if FileAccess.file_exists(path3):
+		part3(ECodes.string_from_file(path3), answer3)
 
 
 func _on_main_pressed() -> void:
-    get_tree().change_scene_to_file("res://scenes/main_entry.tscn") 
+	get_tree().change_scene_to_file("res://scenes/main_entry.tscn") 
 
 
 func _on_example_text_edit_3_text_changed() -> void:
-    var file: FileAccess = FileAccess.open(example_path3, FileAccess.WRITE)
-    if file:
-        file.store_string(exampleText3.text)
-        file.close()
-    else:
-        push_error("Error writing to " + example_path3)
+	var file: FileAccess = FileAccess.open(example_path3, FileAccess.WRITE)
+	if file:
+		file.store_string(exampleText3.text)
+		file.close()
+	else:
+		push_error("Error writing to " + example_path3)
 
 #endregion
 
 func part1(data: String, ans: LineEdit) -> void:
-    var gears: Array[int] = ECodes.array_int_from_string(data)
-    var first_gear: int = gears[0]
-    var last_gear: int = gears[gears.size() -1]
+	var gears: Array[int] = ECodes.array_int_from_string(data)
+	var first_gear: int = gears[0]
+	var last_gear: int = gears[gears.size() -1]
     
-    ans.text = str(int(first_gear * 2025/last_gear ))
+	ans.text = str(int(first_gear * 2025/last_gear ))
     
     
 func part2(data: String, ans: LineEdit) -> void:
-    # 10_000_000_000_000
-    var gears: Array[int] = ECodes.array_int_from_string(data)
-    var first_gear: int = gears[0]
-    var last_gear: int = gears[gears.size() -1]
+	# 10_000_000_000_000
+	var gears: Array[int] = ECodes.array_int_from_string(data)
+	var first_gear: int = gears[0]
+	var last_gear: int = gears[gears.size() -1]
     
-    ans.text = str(int(10_000_000_000_000 * last_gear/first_gear) +1)
+	ans.text = str(int(10_000_000_000_000 * last_gear/first_gear) +1)
 
 
 
 func part3(data: String, ans: LineEdit) -> void:
-    var gears: Array[int] = ECodes.array_int_from_string(data)
-    var ratios: Array[float] = []
-    assert(gears.size() %2 == 0, "Should be an even number of gears remaining")
-    while not gears.is_empty():
-        var gear: int = gears.pop_front()
-        var next_gear:int = gears.pop_front()
-        ratios.append(float(gear)/float(next_gear))
-    var product: float = 100
-    for ratio: float in ratios:
-        product *= ratio
+	var gears: Array[int] = ECodes.array_int_from_string(data)
+	var ratios: Array[float] = []
+	assert(gears.size() %2 == 0, "Should be an even number of gears remaining")
+	while not gears.is_empty():
+		var gear: int = gears.pop_front()
+		var next_gear:int = gears.pop_front()
+		ratios.append(float(gear)/float(next_gear))
+	var product: float = 100
+	for ratio: float in ratios:
+		product *= ratio
     
-    ans.text = str(int(product))
+	ans.text = str(int(product))
