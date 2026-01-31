3d681ed66ba13ffd2d5a0863d183e28044730d3a
Kurt Schwind <kurt.schwind@gmail.com>
2026-01-31 10:22:23 -0600

    union_find.gd

--- 2025/godot/aoc/union_find.gd
+++ 2025/godot/aoc/union_find.gd
@@ -13,27 +13,27 @@ var _rank: Array[int]
 ## Creates n disjoint sets, each containing a single element {0}, {1}, ..., {n-1}
 ## @param n: Number of elements to initialize (elements will be numbered 0 to n-1)
 func _init(n: int) -> void:
-    _parent = []
-    _rank = []
+	_parent = []
+	_rank = []
 
-    var ok1: int = _parent.resize(n)
-    var ok2: int = _rank.resize(n)
+	var ok1: int = _parent.resize(n)
+	var ok2: int = _rank.resize(n)
 
-    assert(ok1 == OK and ok2 == OK)
+	assert(ok1 == OK and ok2 == OK)
 
-    for i: int in n:
-        _parent[i] = i
-        _rank[i] = 0
+	for i: int in n:
+		_parent[i] = i
+		_rank[i] = 0
 
 ## Find the representative (root) of the set containing element x
 ## Uses path compression optimization to flatten the tree structure for faster future queries
 ## @param x: Element to find the representative of (must be in range [0, n-1])
 ## @return: The representative element of the set containing x
 func find(x: int) -> int:
-    if _parent[x] != x:
-        _parent[x] = find(_parent[x])
+	if _parent[x] != x:
+		_parent[x] = find(_parent[x])
 
-    return _parent[x]
+	return _parent[x]
 
 ## Unite (merge) the sets containing elements a and b
 ## Uses union by _rank optimization to keep trees balanced and maintain efficiency
@@ -41,23 +41,23 @@ func find(x: int) -> int:
 ## @param b: Second element (must be in range [0, n-1])
 ## @postcondition: After this operation, a and b will be in the same connected component
 func unite(a: int, b: int) -> void:
-    var ra: int = find(a)
-    var rb: int = find(b)
+	var ra: int = find(a)
+	var rb: int = find(b)
 
-    if ra == rb:
-        return
+	if ra == rb:
+		return
 
-    if _rank[ra] < _rank[rb]:
-        _parent[ra] = rb
-    elif _rank[ra] > _rank[rb]:
-        _parent[rb] = ra
-    else:
-        _parent[rb] = ra
-        _rank[ra] += 1
+	if _rank[ra] < _rank[rb]:
+		_parent[ra] = rb
+	elif _rank[ra] > _rank[rb]:
+		_parent[rb] = ra
+	else:
+		_parent[rb] = ra
+		_rank[ra] += 1
 
 ## Check if two elements are in the same connected component (set)
 ## @param a: First element (must be in range [0, n-1])
 ## @param b: Second element (must be in range [0, n-1])
 ## @return: true if a and b are in the same set, false otherwise
 func same(a: int, b: int) -> bool:
-    return find(a) == find(b)
+	return find(a) == find(b)
