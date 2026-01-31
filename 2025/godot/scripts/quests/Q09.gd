9eb2642982a95b4924bd7a5cfc3c7bb2098ba1ab
Kurt Schwind <kurt.schwind@gmail.com>
2026-01-31 10:20:47 -0600

    Q09.gd

--- 2025/godot/scripts/quests/Q09.gd
+++ 2025/godot/scripts/quests/Q09.gd
@@ -2,12 +2,12 @@ class_name Q09 extends RefCounted
 
 
 static func part1(data: String) -> String:
-    return data
+	return data
     
 
 static func part2(data: String) -> String:
-    return data
+	return data
 
 
 static func part3(data: String) -> String:
-    return data
+	return data
