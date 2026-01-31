2b2ac7044c808c43f59db8c946c33faf3b50d5e0
Kurt Schwind <kurt.schwind@gmail.com>
2026-01-31 10:22:23 -0600

    counter.gd

--- 2025/godot/aoc/counter.gd
+++ 2025/godot/aoc/counter.gd
@@ -5,8 +5,8 @@ var _counts: Dictionary = {}
 ## Creates Counter from an Array
 ## @param Optional Array of items to count
 func _init(arr: Array = []) -> void:
-    for item: Variant in arr:
-        add(item)
+	for item: Variant in arr:
+		add(item)
 
 ## Increase count of an item by `count`
 ## Inserts item if not there, otherwise adds `count`
@@ -14,70 +14,70 @@ func _init(arr: Array = []) -> void:
 ## @param count Option parameter defaults to 1.
 ## @returns void
 func add(item: Variant, count: int = 1) -> void:
-    if _counts.has(item):
-        _counts[item] += count
-    else:
-        _counts[item] = count
+	if _counts.has(item):
+		_counts[item] += count
+	else:
+		_counts[item] = count
 
 ## Get count of an item (returns 0 if not present)
 ## @param item 
 ## @returns int count for given item
 func get_count(item: Variant) -> int:
-    return _counts.get(item, 0)
+	return _counts.get(item, 0)
 
 ## Remove occurrences of an item
 ## @param item Item to subtract `count` from.
 ## @param count Optional parameter number to remove
 func remove(item: Variant, count: int = 1) -> void:
-    if _counts.has(item):
-        _counts[item] -= count
-        if _counts[item] <= 0:
-            _counts.erase(item)
+	if _counts.has(item):
+		_counts[item] -= count
+		if _counts[item] <= 0:
+			_counts.erase(item)
 
 # Get the most common items
 ## @param n Optional parameter to limit size of returned Array
 ## @returns Array (limited to size n if given) sorted by highest count
 func most_common(n: int = -1) -> Array:
-    var arr: Array = []
-    for key: Variant in _counts.keys():
-        arr.append([key, _counts[key]])
+	var arr: Array = []
+	for key: Variant in _counts.keys():
+		arr.append([key, _counts[key]])
     
-    # Sort by count (descending)
-    arr.sort_custom(func(a: Variant, b: Variant) -> bool: return a[1] > b[1])
+	# Sort by count (descending)
+	arr.sort_custom(func(a: Variant, b: Variant) -> bool: return a[1] > b[1])
     
-    if n > 0:
-        return arr.slice(0, n)
-    return arr
+	if n > 0:
+		return arr.slice(0, n)
+	return arr
 
 ## Get total count of all items
 ## @returns int sum of all counts
 func total() -> int:
-    var sum: int = 0
-    for count: Variant in _counts.values():
-        sum += count
-    return sum
+	var sum: int = 0
+	for count: Variant in _counts.values():
+		sum += count
+	return sum
 
 ## Get all unique items
 ## @returns Array of all items being counted
 func items() -> Array:
-    return _counts.keys()
+	return _counts.keys()
 
 ## Clear all counts
 func clear() -> void:
-    _counts.clear()
+	_counts.clear()
 
 ## Check if item exists
 ## @param item Item to check existance of
 ## @returns bool True if item is in Counter
 func has(item: Variant) -> bool:
-    return _counts.has(item)
+	return _counts.has(item)
 
 ## Get the underlying dictionary
 ## @returns copy of underlying dictionary
 func to_dict() -> Dictionary:
-    return _counts.duplicate()
+	return _counts.duplicate()
 
 ## String
 ## @returns String representation of Counter
 func _to_string() -> String:
-    return str(_counts)
+	return str(_counts)
